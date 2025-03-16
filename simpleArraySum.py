fcb14ba3149aedc6b06110242e8a5dfa57d3c9b0
Abdul Moeez Tariq <abdulmoeeztariq716@gmail.com>
2025-03-16 22:45:14 +0500

    easy task

---
+++ simpleArraySum.py
@@ -0,0 +1,33 @@
+#Problem
+#Given an array of integers, find the sum of its elements.
+#For example, if the array ,[1,2,3] , 1+2+3 so return 6 .
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    # Write your code here
+    return sum(ar)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
